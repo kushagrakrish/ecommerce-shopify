eaccea9b2b4859cabc1ec173a2cc0b505109689e
Kushagra Krishna <kushagrakrishna@Kushagras-MacBook-Air.local>
2023-10-18 10:10:24 +0530

    Dependecies installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className='text-9xl font-inter'>Hello</h1>
+    </div>
+  );
 };
 
 export default App;
